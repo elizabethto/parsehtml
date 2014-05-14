6f0442ea902fb4347a578ea731225b4a5e4191d3
sto <son.c.to@gmail.com>
2014-05-13 21:11:49 -0400

    example of using jsoup from clojure

--- src/parsehtml/core.clj
+++ src/parsehtml/core.clj
@@ -1,5 +1,11 @@
 (ns parsehtml.core)
+(import 'org.jsoup.Jsoup)
 
+(def httpConnection (Jsoup/connect "http://www.allmenus.com/ny/new-york/250138-xo-kitchen/menu/"))
+(def document (.get httpConnection))
+(def links (.select document "#menu"))
+
+(println 1)
 (defn foo
   "I don't do a whole lot."
   [x]
