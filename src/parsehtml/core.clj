f001752098a0e240d5be5d78682bae171d64f49d
Elizabeth To <elizabeth.to@gmail.com>
2014-05-16 16:39:28 -0400

    print out all menu items and prices

--- src/parsehtml/core.clj
+++ src/parsehtml/core.clj
@@ -4,13 +4,22 @@
 (def ^org.jsoup.Connection httpConnection (Jsoup/connect "http://www.allmenus.com/ny/new-york/322071-xo-cafe-and-grill/menu/"))
 (def ^org.jsoup.nodes.Document document (.get httpConnection))
 (def ^org.jsoup.nodes.Element navDivTag (.getElementById document "menu"))
+
+(def allText (.text navDivTag))
+
+(doseq [element allText]
+  (prn (element)))
+
 (def ^org.jsoup.select.Elements elements (.getElementsByClass navDivTag "name"))
 (def ^String menuItems (.html elements))
-(doseq [element elements]
 
+(def x (.getAllElements navDivTag))
+
+(doseq [element elements]
   (prn (.html element)))
  
 (for [element elements] element)
 
 (dotimes [element (.size elements)] (prn element))
 
+(print 1)
