d8dae91e9c76f7e245e885383496152688daf811
Elizabeth To <elizabeth.to@gmail.com>
2014-05-16 01:16:17 -0400

    restaurant menu

--- src/parsehtml/core.clj
+++ src/parsehtml/core.clj
@@ -1,12 +1,8 @@
 (ns parsehtml.core)
 (import 'org.jsoup.Jsoup)
 
-(def httpConnection (Jsoup/connect "http://jsoup.org"))
+(def httpConnection (Jsoup/connect "http://www.allmenus.com/ny/new-york/322071-xo-cafe-and-grill/menu/"))
 (def document (.get httpConnection))
-(def navDivTag (.getElementsByClass document "nav-sections"))
-
-(def dummy (.get navDivTag 0))
-
-(def list (.getElementsByTag dummy "a"))
-
-(def topics (.html list))
+(def navDivTag (.getElementById document "menu"))
+(def list (.getElementsByClass navDivTag "name"))
+(def menuItems (.html list))
