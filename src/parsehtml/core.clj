e874ec9f82d6eff79312076790ac98bf3f599b1a
Elizabeth To <elizabeth.to@gmail.com>
2014-05-24 17:51:25 -0400

    created functions

--- src/parsehtml/core.clj
+++ src/parsehtml/core.clj
@@ -45,5 +45,4 @@
 (spit "menu.txt" (apply str finalMenu))
 
 
-
-
+{:appetizers [{:name "salmon" :price 10.95} {:name "chicken wings" :price 6.95}] :dimsum [{:name "shrimp dumpling" :price 3.95} {:name "turnip cake" :price 1.95}] :beverages [{:name "soda" :price 1.25} {:name "green tea" :price 1.25}] }
