9254204935049348a8b6552484c36c58e0a1fc95
Elizabeth To <elizabeth.to@gmail.com>
2014-06-25 16:18:30 -0400

    corrected spit

--- src/parsehtml/test.clj
+++ src/parsehtml/test.clj
@@ -60,7 +60,7 @@
             full-menu-map item-map]
         (println "NEW CATEGORY")
         (println item-map)
-        (spit "fullmenu.txt" (apply str item-map))
+        (spit "fullmenu.txt" (pr-str item-map))
         item-map))))
 
 
